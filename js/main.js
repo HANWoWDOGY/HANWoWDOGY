5f3c68f3f7a6cf0f74561c0adde7a75b9c675c8f
HANWoWDOGY <HANWoWDOGY@yandex.ru>
2019-12-18 21:38:33 +0300

    add new section

--- js/main.js
+++ js/main.js
@@ -47,4 +47,36 @@ $(document).ready(function () {
 
   new WOW().init();
 
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Укажите ваше имя",
+        minlength: "Имя не менее двух букв"
+      }, 
+      userPhone: "Укажите ваш номер телефона",
+      userEmail: {
+        required: "Укажите ваш email ",
+        email: "Пример: name@domain.com"
+      }
+    }
+  });
+
+  //маска для телефона
+
+  $('[type=tel]').mask('+7(000) 000-00-00', {placeholder: '+7(___) ___-__-__'});
+
 });
